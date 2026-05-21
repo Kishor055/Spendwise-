c8747b3acd029f2ffff154c578800ba0042efb3f
Kishor Kakde <kishorkakde026@gmail.com>
2026-05-21 20:33:49 +0530

    Remove apiKey from firebase configuration
    Removed apiKey from firebaseConfig for security reasons.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-8276360353-34cd3",
   "appId": "1:534861469666:web:634d51a88dab0844570bbb",
-  "apiKey": "AIzaSyD4hCyU7QCYACahBIOrhfQPpLojfiWl9HI",
-  "authDomain": "studio-8276360353-34cd3.firebaseapp.com",
+    "authDomain": "studio-8276360353-34cd3.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "534861469666"
 };
