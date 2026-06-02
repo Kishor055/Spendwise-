2918697511c1fee1527c96c8414b87a4293bc654
Kishor Kakde Patil <kishorkakde026@gmail.com>
2026-06-02 20:11:24 +0000

    Initial commit

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,12 @@
 export const firebaseConfig = {
   "projectId": "studio-8276360353-34cd3",
   "appId": "1:534861469666:web:634d51a88dab0844570bbb",
+<<<<<<< HEAD
     "authDomain": "studio-8276360353-34cd3.firebaseapp.com",
+=======
+  "apiKey": "AIzaSyAQre8JmpTBuPmaUrqrRosG7xObh-tj85k",
+  "authDomain": "studio-8276360353-34cd3.firebaseapp.com",
+>>>>>>> 2e58f81 (add gitingonor in this add config.ts file to make api key private)
   "measurementId": "",
   "messagingSenderId": "534861469666"
 };
